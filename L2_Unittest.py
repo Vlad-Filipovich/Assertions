6ae6e9c7f626ced00a305416275364d6155b712e
Vlad Filipovich <fvfordota2@gmail.com>
2022-12-12 21:57:48 +0300

    + L2_Unittest.py

---
+++ L2_Unittest.py
@@ -0,0 +1,3 @@
+# unittest - python module with testing framework
+# unit testing - common conception of testing small pieces of your code
+# all code is written in notepad and was run through terminal (D:\PythonTest)
